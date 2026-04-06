2a6b53a5685d636091bfbfe877a5731f7bd5fa7c
unknown <zaky.shadra.tif25@polban.ac.id>
2026-04-06 17:06:28 +0700

    add feature delete line

--- holynano.c
+++ holynano.c
@@ -10,6 +10,13 @@ char buffer[MAX_ROWS][MAX_COLS];
 int cursor_x = 0, cursor_y = 0;
 int num_rows = 0;
 struct termios orig_termios;
+char clipboard[MAX_COLS];
+
+int select_start = -1;
+int select_end = -1;
+
+char clipboard_block[MAX_ROWS][MAX_COLS];
+int clipboard_lines = 0;
 
 void disableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
@@ -47,6 +54,30 @@ void refresh_screen() {
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", cursor_y + 1, cursor_x + 1);
   write(STDOUT_FILENO, buf, strlen(buf));
 }
+
+void deleteLine() {
+  if (num_rows == 0) return;
+
+  if (num_rows == 1) {
+    buffer[0][0] = '\0';
+    cursor_x = 0;
+    cursor_y = 0;
+    return;
+  }
+
+  for (int i = cursor_y; i < num_rows - 1; i++) {
+    strcpy(buffer[i], buffer[i + 1]);
+  }
+
+  num_rows--;
+  buffer[num_rows][0] = '\0';
+
+  if (cursor_y >= num_rows) cursor_y = num_rows - 1;
+
+  int len = strlen(buffer[cursor_y]);
+  if (cursor_x > len) cursor_x = len;
+}
+
 int main() {
   enableRawMode();
   
@@ -61,26 +92,62 @@ int main() {
       read(STDIN_FILENO, &c, 1);  // read [
       read(STDIN_FILENO, &c, 1);  // read A/B/C/D
       
-      if (c == 'A' && cursor_y > 0) cursor_y--;           // up
-      else if (c == 'B' && cursor_y < num_rows - 1) cursor_y++;  // down
-      else if (c == 'C') cursor_x++;                       // right
-      else if (c == 'D' && cursor_x > 0) cursor_x--;       // left
+      if (c == 'A') { // up
+        if (cursor_y > 0) cursor_y--;
+        int len = strlen(buffer[cursor_y]);
+        if (cursor_x > len) cursor_x = len;
+      }
+      else if (c == 'B') { // down
+        if (cursor_y < num_rows - 1) cursor_y++;
+        int len = strlen(buffer[cursor_y]);
+        if (cursor_x > len) cursor_x = len;
+      }
+      else if (c == 'C') { // right
+        int len = strlen(buffer[cursor_y]);
+        if (cursor_x < len) cursor_x++;
+        else if (cursor_y < num_rows - 1) { cursor_y++; cursor_x = 0; }
+      }
+      else if (c == 'D') { // left
+        if (cursor_x > 0) cursor_x--;
+        else if (cursor_y > 0) { cursor_y--; cursor_x = strlen(buffer[cursor_y]); }
+      }
     }
     else if (c == 24) {  // Ctrl+X
       write(STDOUT_FILENO, "\x1b[2J\x1b[H", 7);
       break;
     }
+    else if (c == 20) {  // Ctrl+T (delete line)
+      deleteLine();
+    }
     else if (c >= 32 && c < 127) {  // printable char
       int len = strlen(buffer[cursor_y]);
       if (len < MAX_COLS - 1) {
-        buffer[cursor_y][len] = c;
-        buffer[cursor_y][len + 1] = '\0';
+        if (cursor_x < len) {
+          memmove(&buffer[cursor_y][cursor_x + 1], &buffer[cursor_y][cursor_x], len - cursor_x + 1);
+          buffer[cursor_y][cursor_x] = c;
+        } else {
+          buffer[cursor_y][len] = c;
+          buffer[cursor_y][len + 1] = '\0';
+        }
         cursor_x++;
       }
     }
     else if (c == '\r' || c == '\n') {  // Enter in raw mode is usually '\r'
-      if (num_rows < MAX_ROWS - 1) {
-        buffer[num_rows][0] = '\0';
+      if (num_rows < MAX_ROWS) {
+        // shift lines down
+        for (int i = num_rows; i > cursor_y + 1; i--) {
+          strncpy(buffer[i], buffer[i - 1], MAX_COLS - 1);
+          buffer[i][MAX_COLS - 1] = '\0';
+        }
+        // move suffix of current line to new line
+        int len = strlen(buffer[cursor_y]);
+        if (cursor_x <= len) {
+          strncpy(buffer[cursor_y + 1], &buffer[cursor_y][cursor_x], MAX_COLS - 1);
+          buffer[cursor_y + 1][MAX_COLS - 1] = '\0';
+          buffer[cursor_y][cursor_x] = '\0';
+        } else {
+          buffer[cursor_y + 1][0] = '\0';
+        }
         num_rows++;
         cursor_y++;
         cursor_x = 0;
@@ -90,9 +157,24 @@ int main() {
       if (cursor_x > 0) {
         int len = strlen(buffer[cursor_y]);
         if (cursor_x <= len) {
-          memmove(&buffer[cursor_y][cursor_x - 1], 
+          memmove(&buffer[cursor_y][cursor_x - 1],
                   &buffer[cursor_y][cursor_x], len - cursor_x + 1);
           cursor_x--;
+        }
+      } else if (cursor_y > 0) {
+        // join current line to previous if space permits
+        int prev_len = strlen(buffer[cursor_y - 1]);
+        int cur_len = strlen(buffer[cursor_y]);
+        if (prev_len + cur_len < MAX_COLS) {
+          strncat(buffer[cursor_y - 1], buffer[cursor_y], MAX_COLS - 1 - prev_len);
+          // shift lines up
+          for (int i = cursor_y; i < num_rows - 1; i++) {
+            strncpy(buffer[i], buffer[i + 1], MAX_COLS - 1);
+            buffer[i][MAX_COLS - 1] = '\0';
+          }
+          num_rows--;
+          cursor_y--;
+          cursor_x = prev_len;
         }
       }
     }
