0039c0cbc40cc43ac47aac8adc8ea076a448621a
unknown <zaky.shadra.tif25@polban.ac.id>
2026-04-07 15:33:11 +0700

    change to modular

---
+++ basicLine.h
@@ -0,0 +1,11 @@
+#ifndef BASICLINE_H
+#define BASICLINE_H
+
+#include "basicFramework.h"
+
+void deleteLine(void);
+void copyLine(void);
+void pasteLine(void);
+void run_editor_loop(void);
+
+#endif
