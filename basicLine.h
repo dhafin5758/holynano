e89bdc65e499cdf9f16696e38254d1077c72f576
unknown <zaky.shadra.tif25@polban.ac.id>
2026-04-08 17:36:06 +0700

    feature start end paste selection

--- basicLine.h
+++ basicLine.h
@@ -7,5 +7,8 @@ void deleteLine(void);
 void copyLine(void);
 void pasteLine(void);
 void run_editor_loop(void);
+void startSelection(void);
+void endSelection(void);
+void pasteSelection(void);  
 
 #endif
