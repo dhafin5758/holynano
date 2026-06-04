bddcbb946df9cf7b23499299a2ecefa7f875021f
unknown <zaky.shadra.tif25@polban.ac.id>
2026-06-04 17:44:29 +0700

    ubah dhafin.h ke versi main

--- dhafin.h
+++ dhafin.h
@@ -27,30 +27,4 @@ void disableRawMode(void);
 
 #endif
 =======
-#ifndef DHAFIN_H
-#define DHAFIN_H
 
-#include <stddef.h>
-
-typedef struct Node {
-  char *info; //point to string
-  struct Node *prev;
-  struct Node *next;
-} Node;
-
-typedef struct Buffer {
-  Node *head;
-  Node *tail;
-  size_t length; //unsigned int (node length) 
-} Buffer;
-
-void initBuffer(Buffer *buffer);
-Node *makeNode(const char *info);
-int addLine(Buffer *buffer, const char *info);
-int insertAfter(Buffer *buffer, Node *node, const char *info);
-void clearBuffer(Buffer *buffer);
-int enableRawMode(void);
-void disableRawMode(void);
-
-#endif
->>>>>>> 13c565e013847c4452ef284270449744a9fc6b0d:dhafin.h
